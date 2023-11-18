9cbe3e09f6d5ce5b518b3a6edd593e6eb5602520
wada-tomoya <134474403+wada-tomoya@users.noreply.github.com>
2023-11-18 16:15:25 +0900

    wadaのファイル追加

---
+++ Source/BesideShooting/Private/wadaC++/BehaviorTree_wada.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "wadaC++/BehaviorTree_wada.h"
+
---
+++ Source/BesideShooting/public/wadaC++/BehaviorTree_wada.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree_wada.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BESIDESHOOTING_API UBehaviorTree_wada : public UBehaviorTree
+{
+	GENERATED_BODY()
+	
+};
